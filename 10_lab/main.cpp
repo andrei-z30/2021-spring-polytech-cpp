62a68903403b80422fb2355052c566b348833b86
DenisKolodich <70860090+DenisKolodich@users.noreply.github.com>
2021-02-10 23:02:07 +0300

    Add solution

--- 10_lab/main.cpp
+++ 10_lab/main.cpp
@@ -3,6 +3,13 @@
 
 #include <iostream>
 
+int absolute_value(int number){
+    if(number<0)
+        return -number;
+    return number;
+}
 int main() {
+    int x;
+    std::cin>> x;
     std::cout << absolute_value(x) << std::endl;
 }
