9cd96ccf134f5c7b62a645b15f55d2e97168316d
Andrey Zykov <andrei-z30@yandex.ru>
2021-02-11 20:03:00 +0300

    Lab 09 10 11 Done

--- 10_lab/main.cpp
+++ 10_lab/main.cpp
@@ -3,6 +3,16 @@
 
 #include <iostream>
 
+int absolute_value(int num);
+
 int main() {
+    int x;
+    std::cin >> x;
     std::cout << absolute_value(x) << std::endl;
+}
+
+int absolute_value(int num) {
+    if (0 > num)
+        num *= -1;
+    return num;
 }
--- 11_lab/main.cpp
+++ 11_lab/main.cpp
@@ -3,6 +3,52 @@
 
 #include <iostream>
 
+void MaxAndMinValueOfInt_var1();
+void MaxAndMinValueOfInt_var2();
+
 int main() {
-    // Решение тут
+    MaxAndMinValueOfInt_var1();
+    MaxAndMinValueOfInt_var2();
+}
+
+int RunOfValue(int dir) {
+    int num = 0;
+    if (dir)
+        while (num + 1 > num)
+            num++;
+    else
+        while (num - 1 < num)
+            num--;
+    return num;
+}
+
+void MaxAndMinValueOfInt_var1() {
+    std::cout << "Variant 1" << std::endl;
+    int num_max = RunOfValue(1);
+    int num_min = RunOfValue(-1);
+    std::cout << "min: " << num_min << std::endl << "max: " << num_max << std::endl;
+}
+
+void MaxAndMinValueOfInt_var2() {
+    std::cout << "Variant 2" << std::endl;
+    int num = 1;
+    while (num < num * 2) {
+        num *= 2;
+    }
+    while (num < num + 1) {
+        num += 1;
+    }
+    int num_max = num;
+    int num_min = 0;
+    if (num_min > num_min - 1){
+        num = -1;
+        while (num > num * 2) {
+            num *= 2;
+        }
+        while (num > num - 1) {
+            num -= 1;
+        }
+        num_min = num;
+    }
+    std::cout << "min: " << num_min << std::endl << "max: " << num_max << std::endl;
 }
