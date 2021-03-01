703fb266e9fd17bc3c967bba144ab868ca3cd0a2
Andrey Zykov <andrei-z30@yandex.ru>
2021-03-01 09:52:09 +0300

    Lab 16 done

--- 16_lab/main.cpp
+++ 16_lab/main.cpp
@@ -2,7 +2,22 @@
 
 #include <iostream>
 
+int hamming(int num1, int num2);
+
 int main() {
     std::cout << hamming(7, 3) << std::endl;
     return 0;
+}
+
+int hamming(int num1, int num2) {
+    int hamDist{0};
+    while (num1 != 0 | num2 != 0) {
+        int bit1 = num1 % 2;
+        num1 = num1 / 2;
+        int bit2 = num2 % 2;
+        num2 = num2 / 2;
+        if (bit1 != bit2)
+            hamDist++;
+    }
+    return hamDist;
 }
