712affde745d8d87b6e30bba4a3d4786e6e5bc93
DenisKolodich <70860090+DenisKolodich@users.noreply.github.com>
2021-03-01 13:05:00 +0300

    fixed code

--- 16_lab/main.cpp
+++ 16_lab/main.cpp
@@ -4,7 +4,7 @@
 
 int hamming(int num_1, int num_2) {
     int cnt = 0;
-    while (num_1 > 0 && num_2 > 0) {
+    while (num_1 > 0 || num_2 > 0) {
         if ((num_1 % 2) != (num_2 % 2))
             cnt++;
         num_1 /= 2;
