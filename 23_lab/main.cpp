8cd665945b7e7ebedcc4159252fd19346f03dc21
Andrey Zykov <andrei-z30@yandex.ru>
2021-03-27 18:44:34 +0300

    Lab 23 done

--- 23_lab/main.cpp
+++ 23_lab/main.cpp
@@ -2,9 +2,23 @@
 // целыми числами из диапазона от 0 до n. Воспользуйтесь функциями
 // std::rand() и std::srand().
 
+#include <iostream>
 #include <cstdlib>
+#include <ctime>
+
+void generateRndArray(int array[], int size, int maxValue) {
+    for (int i = 0; i < size; ++i) {
+        array[i] = std::rand() % maxValue;
+    }
+}
 
 int main() {
-    // Решение тут
+    std::srand(std::time(nullptr));
+    int maxValue = 6;
+    int array[10] = {0};
+    generateRndArray(array, 10, maxValue);
+    for (int i : array) {
+        std::cout << i << " ";
+    }
     return 0;
 }
