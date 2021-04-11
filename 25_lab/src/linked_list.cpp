e3c08abbafb3447ceaee8f922396172d29c99f06
DenisKolodich <70860090+DenisKolodich@users.noreply.github.com>
2021-04-12 00:45:38 +0300

    Add a part solution

--- 25_lab/src/linked_list.cpp
+++ 25_lab/src/linked_list.cpp
@@ -6,32 +6,60 @@ List duplicate(List &list) {
 }
 
 void print(List &list) {
-    // решение тут
+    ListNode *print_list = list.head;
+    while (print_list->next != nullptr) {
+        std::cout << print_list->data << "->";
+        print_list = print_list->next;
+    }
+    std::cout << print_list->data << "->[x]" << std::endl;
 }
 
 unsigned int size(List &list) {
-    // решение тут
+
 }
 
 
-void delete_list(List &list)
-{
-    // решение тут
-    list.head = nullptr;
+void delete_list(List &list) {
+
 }
 
 
 void add(List &list, int value) {
-    auto* new_node = new ListNode{value, nullptr};
-    // решение тут
+    auto *new_node = new ListNode{value, nullptr};
+
+
 }
 
 bool compare(List &list_a, List &list_b) {
-    // решение тут
+    ListNode *copy_a = list_a.head;
+    ListNode *copy_b = list_b.head;
+    while (copy_a->next != nullptr && copy_b->next != nullptr) {
+        if (copy_a->data != copy_b->data)
+            return false;
+        copy_a = copy_a->next;
+        copy_b = copy_b->next;
+    }
+    return true;
 }
 
 List merge(List &list_a, List &list_b) {
-    // решение тут
+    ListNode *copy_a = list_a.head;
+    ListNode *copy_b = list_b.head;
+    List list_l;
+
+    while (copy_a->next != nullptr || copy_b->next != nullptr) {
+        if (copy_a->next != nullptr) {
+            add(list_l, copy_a->data);
+            copy_a = copy_a->next;
+        }
+        if (copy_b->next != nullptr) {
+            add(list_l, copy_b->data);
+            copy_a = copy_b->next;
+        }
+    }
+
+    return list_l;
+
 }
 
 List concat(List &list_a, List &list_b) {
