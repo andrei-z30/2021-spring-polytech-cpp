3863fdf8a1ac5fcb95fa3501739b84e0dd0adc51
Andrey Zykov <andrei-z30@yandex.ru>
2021-04-11 12:43:24 +0300

    Lab 25 Ver_0.1

--- 25_lab/src/linked_list.cpp
+++ 25_lab/src/linked_list.cpp
@@ -3,6 +3,7 @@
 
 List duplicate(List &list) {
     // решение тут
+    return list;
 }
 
 void print(List &list) {
@@ -10,30 +11,53 @@ void print(List &list) {
 }
 
 unsigned int size(List &list) {
-    // решение тут
+    int size = 0;
+    if (list.head == nullptr) {
+        return 0;
+    }
+    ListNode *current = list.head;
+    while (current != nullptr) {
+        current = current->next;
+        size++;
+    }
+    return size;
 }
 
 
-void delete_list(List &list)
-{
-    // решение тут
-    list.head = nullptr;
+void delete_list(List &list) {
+    while (list.head != nullptr){
+        ListNode *temp = list.head;
+        list.head = list.head->next;
+        delete temp;
+    }
 }
 
 
 void add(List &list, int value) {
     auto* new_node = new ListNode{value, nullptr};
-    // решение тут
+    if (list.head == nullptr) {
+        list.head = new_node;
+    } else {
+        ListNode *current = list.head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = new_node;
+    }
+
 }
 
 bool compare(List &list_a, List &list_b) {
     // решение тут
+    return true;
 }
 
 List merge(List &list_a, List &list_b) {
     // решение тут
+    return list_a;
 }
 
 List concat(List &list_a, List &list_b) {
     // решение тут
+    return list_a;
 }
